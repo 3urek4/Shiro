0d5219d1f2e3e1a7a28d287eb32af2cbdaefbf95
GuoHuan <2676391467@qq.com>
2025-01-04 10:38:08 +0800

    test

--- .history/src/components/ui/transition/FadeInOutTransitionView_20250104103058.tsx
+++ .history/src/components/ui/transition/FadeInOutTransitionView_20250104103058.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import type { FC, PropsWithChildren } from 'react'
+
 import { createTransitionView } from './factor'
 import type { BaseTransitionProps } from './typings'
 
-export const FadeInOutTransitionView: FC<PropsWithChildren<BaseTransitionProps>> = createTransitionView({
+export const FadeInOutTransitionView: FC<
+  PropsWithChildren<BaseTransitionProps>
+> = createTransitionView({
   from: {
     opacity: 0.001,
   },
